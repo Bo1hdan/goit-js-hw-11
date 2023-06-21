f3fb0ed537a4a186f78564ae936065d0ec8c9d3e
Bo1hdan <87267253+Bo1hdan@users.noreply.github.com>
2023-06-21 12:46:16 -0600

    done

--- src/index.js
+++ src/index.js
@@ -1,96 +1,54 @@
-// const KEY = '37600791-d09d47700ee4db7cdd78bc1fd';
-
-// const form = document.querySelector('#search-form');
-// const input = document.querySelector('input');
-// const btn = document.querySelector('button');
-// const gallery = document.querySelector('.gallery');
-// let searchValue = '';
-// let searchImg = '';
-
-// form.addEventListener('submit', e => {
-//   e.preventDefault();
-//   searchValue = input.value;
-
-//   SearchImages();
-// });
-
-// async function SearchImages() {
-//   try {
-//     const response = await fetch(
-//       `https://pixabay.com/api/?key=${KEY}&q=${searchValue}&image_type=photo&orientation=horizontal&safesearch=true`
-//     );
-//     const image = await response.json();
-//     const data = image.hits;
-//     data.forEach(element => {
-//       let largeImage = element.largeImageURL;
-//       let smallImage = element.webformatURL;
-//       let description = element.tags;
-//       let likes = element.likes;
-//       let views = element.views;
-//       let comments = element.comments;
-//       let downloads = element.downloads;
-//       let galleryElement = `<div class="photo-card">
-//   <img src="${smallImage}" alt="${description}" loading="lazy" />
-//   <div class="info">
-//     <p class="info-item">
-//       <b>${likes}</b>
-//     </p>
-//     <p class="info-item">
-//       <b>${views}</b>
-//     </p>
-//     <p class="info-item">
-//       <b>${comments}</b>
-//     </p>
-//     <p class="info-item">
-//       <b>${downloads}</b>
-//     </p>
-//   </div>
-// </div>`;
-//       return galleryElement;
-//     });
-//     gallery.innerHTML = galleryElement;
-//   } catch (error) {
-//     console.log(error);
-//   }
-// }
-
-// async function showImg() {
-//   try {
-//     const res = await fetch(`${searchImg}`);
-//     const blob = await res.blob();
-//     const imgUrl = URL.createObjectURL(blob);
-
-//     const imgElement = document.createElement('img');
-//     imgElement.src = imgUrl;
-//     document.body.appendChild(imgElement);
-//   } catch (error) {
-//     console.log(error);
-//   }
-// }
-
-// --------------------------------------------------------------------------------
 const KEY = '37600791-d09d47700ee4db7cdd78bc1fd';
+import Notiflix from 'notiflix';
 
 const form = document.querySelector('#search-form');
 const input = document.querySelector('input');
 const gallery = document.querySelector('.gallery');
+const loadMore = document.querySelector('.load-more');
+const endMessage = document.querySelector('.end-message');
+const noImagesMessage = document.querySelector('.no-images-message');
+let searchValue = '';
+let currentPage = 1;
+let totalHits = 0;
+let isButtonVisible = false;
 
 form.addEventListener('submit', e => {
   e.preventDefault();
-  searchValue = input.value;
+  searchValue = input.value.trim();
+
+  if (searchValue === '') {
+    Notiflix.Notify.failure(`${noImagesMessage.textContent}`);
+    loadMore.style.display = 'none';
+    isButtonVisible = false;
+    return;
+  }
+  currentPage = 1;
+  gallery.innerHTML = '';
 
   SearchImages();
+  isButtonVisible = true;
 });
 
 async function SearchImages() {
   try {
     const response = await fetch(
-      `https://pixabay.com/api/?key=${KEY}&q=${searchValue}&image_type=photo&orientation=horizontal&safesearch=true`
+      `https://pixabay.com/api/?key=${KEY}&q=${searchValue}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${currentPage}`
     );
     const image = await response.json();
-    const data = image.hits;
+
+    if (image.hits.length === 0) {
+      gallery.innerHTML = '';
+      loadMore.style.display = 'none';
+      Notiflix.Notify.failure(`${noImagesMessage.textContent}`);
+      loadMore.style.display = 'none';
+      isButtonVisible = false;
+
+      return;
+    }
+    totalHits = image.totalHits;
+
     let galleryElement = '';
-    data.forEach(element => {
+    image.hits.forEach(element => {
       let largeImage = element.largeImageURL;
       let smallImage = element.webformatURL;
       let description = element.tags;
@@ -102,22 +60,40 @@ async function SearchImages() {
   <img src="${smallImage}" alt="${description}" loading="lazy" />
   <div class="info">
     <p class="info-item">
-      <b>Likes${likes}</b>
+      <b>Likes: ${likes}</b>
     </p>
     <p class="info-item">
-      <b>Views${views}</b>
+      <b>Views: ${views}</b>
     </p>
     <p class="info-item">
-      <b>Comments${comments}</b>
+      <b>Comments: ${comments}</b>
     </p>
     <p class="info-item">
-      <b>Downloads${downloads}</b>
+      <b>Downloads: ${downloads}</b>
     </p>
   </div>
 </div>`;
     });
-    gallery.innerHTML = galleryElement;
+
+    gallery.innerHTML += galleryElement;
+    noImagesMessage.style.display = 'none';
+    if (currentPage === 1 && isButtonVisible) {
+      loadMore.style.display = 'block';
+      endMessage.style.display = 'none';
+    }
+    if (currentPage * 40 >= totalHits) {
+      loadMore.style.display = 'none';
+
+      Notiflix.Notify.failure(`${endMessage.textContent}`);
+    } else {
+      endMessage.style.display = 'none';
+    }
   } catch (error) {
     console.log(error);
   }
 }
+
+loadMore.addEventListener('click', async () => {
+  currentPage++;
+  await SearchImages();
+});
